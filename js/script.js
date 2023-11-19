85bb733405f8559093d54383ff2c5cb730491ae9
Marian4f <mf485725@gmail.com>
2023-11-19 00:27:17 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -2,10 +2,33 @@ function openNav() {
     var x = document.getElementById("navigation");
 
     if (x.className === "navigation") {
-        x.className += " menuJs";  
+        x.className += " menuJs";
         document.getElementById("threeline-icon").innerHTML = "&Cross;";
     } else {
         x.className = "navigation";
         document.getElementById("threeline-icon").innerHTML = "&#9776;";
     }
 }
+document.addEventListener("DOMContentLoaded", function () {
+    const container = document.querySelector('.imgprojeto-container');
+    const images = document.querySelectorAll('.imgprojeto img');
+    let currentIndex = 0;
+
+    function showImage(index) {
+        images.forEach(img => img.style.display = 'none');
+        images[index].style.display = 'block';
+    }
+
+    function nextImage() {
+        currentIndex = (currentIndex + 1) % images.length;
+        showImage(currentIndex);
+    }
+
+    function prevImage() {
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
+        showImage(currentIndex);
+    }
+    showImage(currentIndex);
+    document.querySelector('.btn2').addEventListener('click', nextImage);
+    document.querySelector('.btn1').addEventListener('click', prevImage);
+});
